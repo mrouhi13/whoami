32b99d266ee5016672612aefdc6e62e549ac6a6e
Majid Rouhi Kh <mrouhi13@icloud.com>
2017-11-26 13:53:16 +0330

    Add users api

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from api.serializers import UserSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API ednpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
