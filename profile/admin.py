0b5e7c6359e872194c81c9d2d75671e376bc1860
Majid Rouhi Kh <mrouhi13@icloud.com>
2018-01-15 12:57:05 +0330

    Change user model view in admin site

--- profile/admin.py
+++ profile/admin.py
@@ -1,13 +1,28 @@
 from django.contrib import admin
-from .models import User
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import ugettext_lazy as _
+
+from profile.models import *
 
 # Register your models here.
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'is_staff', 'is_superuser',
-                    'is_active', 'date_joined')
-    date_hierarchy = 'date_joined'
-    ordering = ('-date_joined',)
-    pass
+class UserAdmin(DjangoUserAdmin):
+    """Define admin model for custom User model."""
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff',
+                                       'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'is_staff',)
+    search_fields = ('email',)
+    ordering = ('date_joined',)
