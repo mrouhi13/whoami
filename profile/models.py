32f8201b786233560cf62c98d704bd86dbd65add
Majid Rouhi Kh <mrouhi13@icloud.com>
2018-01-15 13:44:54 +0330

    Improve models design

--- profile/models.py
+++ profile/models.py
@@ -1,13 +1,26 @@
 from django.db import models
-from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin
-from django.core.mail import send_mail
-from django.utils import timezone
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_user_profile(sender, instance=None, created=False, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def save_user_profile(sender, instance=None, **kwargs):
+    instance.profile.save()
+
+
+def upload_to(instance, filename):
+    return 'gallery/{}/{}'.format(instance.owner_id, filename)
 
 
 class UserManager(BaseUserManager):
@@ -15,7 +28,7 @@ class UserManager(BaseUserManager):
 
     def _create_user(self, email, password, **extra_fields):
         """
-        Creates and saves a User with the given email and password.
+        Create and save a user with the given email and password.
         """
         if not email:
             raise ValueError('The given email must be set')
@@ -25,7 +38,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
         return self._create_user(email, password, **extra_fields)
@@ -42,31 +55,20 @@ class UserManager(BaseUserManager):
         return self._create_user(email, password, **extra_fields)
 
 
-class User(AbstractBaseUser, PermissionsMixin):
+class User(AbstractUser):
     """
-    An abstract base class implementing a fully featured User model with
+    An abstract class implementing a fully featured User model with
     admin-compliant permissions.
 
-    Username and password are required. Other fields are optional.
+    Email and password are required. Other fields are optional.
     """
-    email = models.EmailField(_('email address'), unique=True, null=True)
-    is_active = models.BooleanField(
-        _('active'),
-        default=True,
-        help_text=_(
-            'Designates whether this user should be treated as active. '
-            'Unselect this instead of deleting accounts.'
-        ),
-    )
-    is_staff = models.BooleanField(
-        _('staff status'),
-        default=False,
-        help_text=_(
-            'Designates whether the user can log into this admin site.'),
-    )
-    last_login = models.DateTimeField(
-        verbose_name='last login', blank=True, null=True)
-    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    first_name = None
+    last_name = None
+    username = None
+    email = models.EmailField(_('email address'), unique=True,
+                              error_messages={
+        'unique': _("A user with that email already exists."), })
 
     objects = UserManager()
 
@@ -74,82 +76,33 @@ class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
-    class Meta:
-        verbose_name = _('user')
-        verbose_name_plural = _('users')
-
-    def clean(self):
-        super(User, self).clean()
-        self.email = self.__class__.objects.normalize_email(self.email)
-
-    def get_full_name(self):
-        """
-        Returns the email.
-        """
-        full_name = '%s' % (self.email)
-        return full_name.strip()
-
-    def get_short_name(self):
-        "Returns the short name for the user."
-        return self.email
-
-    def email_user(self, subject, message, from_email=None, **kwargs):
-        """
-        Sends an email to this User.
-        """
-        send_mail(subject, message, from_email, [self.email], **kwargs)
-
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    GENDER_NO_BINARY = 'N'
+    GENDER_MALE = 'M'
+    GENDER_FEMALE = 'F'
+    GENDER_CHOICES = (
+        (GENDER_NO_BINARY, _('no-binary')),
+        (GENDER_MALE, _('male')),
+        (GENDER_FEMALE, _('female')),
+    )
+
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(_('first name'), max_length=30, blank=True)
     last_name = models.CharField(_('last name'), max_length=30, blank=True)
     mobile = models.CharField(max_length=30, blank=True)
     bio = models.TextField(max_length=500, blank=True)
     birth_date = models.DateField(_('birth date'), null=True, blank=True)
+    gender = models.CharField(
+        _('gender'), max_length=1, choices=GENDER_CHOICES,
+        default=GENDER_NO_BINARY)
+    avatar = models.ImageField(upload_to=upload_to)
+    country = models.CharField(max_length=30, blank=True)
 
 
-class OrganizerManager(BaseUserManager):
-    def create_organizer(self, email, password):
-        """
-        Creates and saves a organizer with the given email and password.
-        """
-        if not email:
-            raise ValueError('Organizer must have an email address')
-        if not password:
-            raise ValueError('Organizer must have a password')
-
-        organizer = self.model(email=self.normalize_email(email))
-        organizer.set_password(password)
-        organizer.save(using=self._db)
-
-        return organizer
-
-
-class Organizer(models.Model):
+class Organizer(User):
     title = models.CharField(
         verbose_name='organization title',
         max_length=64,
         blank=False
     )
-    email = models.EmailField(
-        verbose_name='email address',
-        max_length=255,
-        unique=True,
-    )
-    mobile = models.CharField(max_length=30, blank=True)
-    is_active = models.BooleanField(
-        verbose_name='active', default=False)
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(
-        verbose_name='last login', blank=True, null=True)
-    date_joined = models.DateField(
-        verbose_name='date joined', auto_now_add=True)
-
-    objects = OrganizerManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
-
-    def __str__(self):
-        return self.email
