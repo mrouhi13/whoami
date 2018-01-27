52b04d1bd5c6b111c344fb08f96731a5b51e921c
Majid Rouhi Kh <mrouhi13@icloud.com>
2018-01-27 11:11:46 +0330

    Add route for views.

--- profile/urls.py
+++ profile/urls.py
@@ -2,12 +2,20 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path, include
 
+from profile import views
 from profile.api.v1 import urls as v1_urls
 
 # Create your urls here.
 
+app_name = 'profile'
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('v1/', include(v1_urls, namespace='default')),
+    path('', views.index, name='index'),
+    path('profile', views.profile, name='profile'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('recover-password', views.recover_password, name='recover_password'),
+    path('agreement', views.agreement, name='agreement'),
+    path('api/v1/', include(v1_urls, namespace='default')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
