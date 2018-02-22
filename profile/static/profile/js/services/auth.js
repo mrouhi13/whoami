84c41766aba5ae180c875c53b06f57f47d0b70ae
mython <mrouhi13@icloud.com>
2018-02-22 15:37:57 +0330

    Add 'signup' function.

--- profile/static/profile/js/services/auth.js
+++ profile/static/profile/js/services/auth.js
@@ -6,6 +6,12 @@ app.factory('Auth', function ($http, Cookie, APP_CONFIG) {
                     return result;
                 });
         },
+        signup: function (credentials) {
+            return $http.post(APP_CONFIG.apiUrl + 'signup/', credentials)
+                .then(function (result) {
+                    return result;
+                });
+        },
         signout: function () {
             return $http.post(APP_CONFIG.apiUrl + 'signout/', {}, {
                 headers: {'Authorization': 'Bearer ' + Cookie.get('token')},
