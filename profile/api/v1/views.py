f02d2fabd18449a2b78045cf2928dc188a51c2a0
mython <mrouhi13@icloud.com>
2018-02-22 20:54:45 +0330

    Remove unusable views.

--- profile/api/v1/views.py
+++ profile/api/v1/views.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.models import update_last_login
 from django.utils.decorators import method_decorator
 from django.views.decorators.debug import sensitive_post_parameters
-from rest_framework import viewsets, permissions, parsers, renderers, status
+from rest_framework import parsers, renderers
 from rest_framework.views import APIView
 
+from profile import viewsets
 from profile.api.v1.serializers import *
 from profile.models import AuthToken
-from profile.permissions import IsOwner
 from profile.response import response
 
 # Create your views here.
@@ -35,9 +35,7 @@ class ObtainAuthToken(APIView):
 
         if token.expired():
             token.delete()
-            token = AuthToken.objects.create(
-                user=serializer.validated_data['user']
-            )
+            token = AuthToken.objects.create(user=user)
 
         update_last_login(None, user)
         return response(content={'token': token.key},
@@ -110,42 +108,3 @@ class RegisterNewUser(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserRegisterSerializer
     http_method_names = ['post']
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed.
-    """
-    permission_classes = (permissions.IsAuthenticated,)
-    queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
-    http_method_names = ['get']
-
-    def get_queryset(self):
-        """
-        This view should return a list of all the achievements
-        for the currently authenticated user.
-        """
-        user = self.request.user
-        return get_user_model().objects.filter(email=user)
-
-
-class ProfileViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows profiles to be viewed or edited.
-    """
-    permission_classes = (IsOwner, permissions.IsAuthenticated)
-    queryset = Profile.objects.all()
-    serializer_class = ProfileSerializer
-    http_method_names = ['get', 'put']
-
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
-    def get_queryset(self):
-        """
-        This view should return a profile for
-        the currently authenticated user.
-        """
-        owner = self.request.user
-        return Profile.objects.filter(owner=owner)
