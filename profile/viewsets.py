881f404668d67dd4ae86e5af872eca38b8723585
mython <mrouhi13@icloud.com>
2018-02-22 20:43:08 +0330

    Customize Viewsets for change response structure.

---
+++ profile/viewsets.py
@@ -0,0 +1,16 @@
+from rest_framework import viewsets
+
+from profile import mixins
+
+
+class ModelViewSet(mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.DestroyModelMixin,
+                   mixins.ListModelMixin,
+                   viewsets.GenericViewSet):
+    """
+    A viewset that provides default `create()`, `retrieve()`, `update()`,
+    `partial_update()`, `destroy()` and `list()` actions.
+    """
+    pass
