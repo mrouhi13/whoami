ee81fafab16cb3456300d973225c25d28f78127d
mython <mrouhi13@icloud.com>
2018-02-22 15:42:54 +0330

    Rename some routes and add 'ResetPassword' routes.

--- profile/api/v1/urls.py
+++ profile/api/v1/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
+
 from profile.api.v1 import views
 
 # Create your urls here.
-
 
 # Create a router and register our viewsets with it.
 app_name = "profile"
@@ -15,6 +15,8 @@ router.register('profiles', views.ProfileViewSet)
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('signin/', views.signin),
-    path('signout/', views.signout),
+    path('signin/', views.ObtainAuthToken.as_view()),
+    path('signout/', views.DisperseAuthToken.as_view()),
+    path('password-reset/', views.PasswordReset.as_view()),
+    path('password-reset/confirm/', views.PasswordResetConfirm.as_view()),
 ] + router.urls
