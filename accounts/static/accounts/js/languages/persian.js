0352b95e97fc54d1b99de37bd603dbac62932a06
Majid Rouhi Kh <mrouhi13@icloud.com>
2018-03-22 22:12:54 +0430

    Message localization added.

---
+++ accounts/static/accounts/js/languages/persian.js
@@ -0,0 +1,10 @@
+app.constant("MESSAGES", {
+    EMAIL_IS_BLANK: 'ایمیل وارد نشده است.',
+    CONFIRM_EMAIL_IS_BLANK: 'تکرار ایمیل وارد نشده است.',
+    PASSWORD_IS_BLANK: 'گذرواژه وارد نشده است.',
+    EMAIL_VALIDATION_ERROR: 'ایمیل معتبر نیست.',
+    CONFIRM_EMAIL_VALIDATION_ERROR: 'تکرار ایمیل معتبر نیست.',
+    PASSWORD_MISMATCH_ERROR: 'ایمیل‌ها یکسان نیست.',
+    UPLOAD_FILE_ERROR: 'فایل ارسال شده معتبر نیست.',
+    FILE_SIZE_ERROR: 'حجم فایل بیش از ۱ مگابایت است.'
+});
