5638c6fc29b24cb7c4774901796db422dea88a5c
Majid Rouhi Kh <mrouhi13@icloud.com>
2018-03-22 22:12:17 +0430

    All urls constants created.

--- accounts/static/accounts/js/app.js
+++ accounts/static/accounts/js/app.js
@@ -1,8 +1,33 @@
+/**
+ * @author Majid Rouhi Kh. <mrouhi13@icloud.com>
+ * @version 0.0.2
+ * @date Dec 2017
+ */
+
 'use strict';
 var app = angular.module('profile', ['ngFileUpload']);
+var domain = 'http://127.0.0.1:8000/';
+var accountsApiBaseUrl = 'accounts/v1/';
 
-app.constant("APP_CONFIG", {
-    appVrsion: '0.0.1',
-    appProducer: 'Majid Rouhi Kh.',
-    apiUrl: "http://127.0.0.1:8000/accounts/v1/"
+app.constant('APP_URLS', {
+    index: domain + '',
+    profile: domain + 'me/',
+    signup: domain + 'signup/',
+    signin: domain + 'signin/',
+    resetPassword: domain + 'password/reset/',
+    resetPasswordConfirm: domain + 'password/reset/confirm/'
+});
+
+app.constant('API_URLS', {
+    profile: domain + accountsApiBaseUrl + 'me/',
+    account: domain + accountsApiBaseUrl + 'account/',
+    createUser: domain + accountsApiBaseUrl + 'users/create/',
+    deleteUser: domain + accountsApiBaseUrl + 'users/delete/',
+    activateUser: domain + accountsApiBaseUrl + 'users/activate/',
+    setEmail: domain + accountsApiBaseUrl + 'email/',
+    setPassword: domain + accountsApiBaseUrl + 'password/',
+    resetPassword: domain + accountsApiBaseUrl + 'password/reset/',
+    resetPasswordConfirm: domain + accountsApiBaseUrl + 'password/reset/confirm/',
+    createToken: domain + accountsApiBaseUrl + 'token/create/',
+    destroyToken: domain + accountsApiBaseUrl + 'token/destroy/'
 });
