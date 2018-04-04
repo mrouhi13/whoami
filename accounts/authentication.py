7d5c48cf66bf12692dd98403833411973d2f76c9
Majid Rouhi Kh <mrouhi13@icloud.com>
2018-04-04 20:39:42 +0430

    Translate error messages and customize django password validation because of it's error messages.

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -1,17 +1,50 @@
 from rest_framework import exceptions
-from rest_framework.authentication import TokenAuthentication
+from rest_framework.authentication import TokenAuthentication, get_authorization_header
 
 from accounts.constants import CustomMessages as Messages
 from accounts.models import AuthToken
 
 
 class CustomTokenAuthentication(TokenAuthentication):
+    """
+    Simple token based authentication.
+
+    Clients should authenticate by passing the token key in the "Authorization"
+    HTTP header, prepended with the string "Token ".  For example:
+
+        Authorization: Token 401f7ac837da42b97f613d789819ff93537bee6a
+    """
+
     keyword = 'Bearer'
     model = AuthToken
 
+    """
+    A custom token model may be used, but must have the following properties.
+
+    * key -- The string identifying the token
+    * user -- The user to which the token belongs
+    """
+
+    def authenticate(self, request):
+        auth = get_authorization_header(request).split()
+
+        if not auth or auth[0].lower() != self.keyword.lower().encode():
+            return None
+
+        if len(auth) == 1:
+            raise exceptions.AuthenticationFailed(Messages.INVALID_TOKEN_HEADER_ERROR_1)
+        elif len(auth) > 2:
+            raise exceptions.AuthenticationFailed(Messages.INVALID_TOKEN_HEADER_ERROR_2)
+
+        try:
+            token = auth[1].decode()
+        except UnicodeError:
+            raise exceptions.AuthenticationFailed(Messages.INVALID_TOKEN_HEADER_ERROR_3)
+
+        return self.authenticate_credentials(token)
+
     def authenticate_credentials(self, key):
         model = self.get_model()
-
         try:
             token = model.objects.select_related('user').get(key=key)
         except model.DoesNotExist:
