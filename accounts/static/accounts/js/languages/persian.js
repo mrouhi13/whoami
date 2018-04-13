2d70efe873fe055324c5a3059cea788768fced21
mrouhi13 <mrouhi13@icloud.com>
2018-04-13 16:43:52 +0430

    Fix minor bug.

--- accounts/static/accounts/js/languages/persian.js
+++ accounts/static/accounts/js/languages/persian.js
@@ -6,7 +6,7 @@ app.constant("MESSAGES", {
     CONFIRM_EMAIL_VALIDATION_ERROR: 'تکرار ایمیل معتبر نیست.',
     PASSWORD_MISMATCH_ERROR: 'ایمیل‌ها یکسان نیست.',
     UPLOAD_FILE_ERROR: 'فایل ارسال شده معتبر نیست.',
-    FILE_SIZE_ERROR: 'حجم فایل بیش از ۱ مگابایت است.'
+    FILE_SIZE_ERROR: 'حجم فایل بیش از ۱ مگابایت است.',
     REGISTRATION_SUCCESSFUL: 'پروفایل شما با موفقیت ایجاد شد.',
     REMOVAL_SUCCESSFUL: 'پروفایل شما با موفقیت حذف شد.'
 });
